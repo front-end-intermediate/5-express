eb4d49de69999cc758bb07f0aaac69b91b072dda
Daniel Deverell <daniel.deverell@gmail.com>
2021-11-16 17:22:59 -0500

    done

---
+++ public/js/scripts.js
@@ -0,0 +1,45 @@
+function getRecipes() {
+  document.querySelector(".recipes").innerHTML = ``;
+  fetch(`api/recipes`)
+    .then((response) => response.json())
+    .then((recipes) => renderRecipes(recipes));
+}
+
+function renderRecipes(recipes) {
+  recipes.forEach((recipe) => {
+    recipeEl = document.createElement("div");
+    recipeEl.innerHTML = `
+      <img src="img/${recipe.image}" />
+      <h3>${recipe.title}</h3>
+      <p>${recipe.description}</p>
+    `;
+    document.querySelector(".recipes").append(recipeEl);
+  });
+}
+
+getRecipes();
+
+function addRecipe(event) {
+  event.preventDefault();
+
+  const { title, image, description } = event.target;
+
+  const recipe = {
+    title: title.value,
+    image: image.value,
+    description: description.value,
+  };
+
+  fetch("api/recipes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(recipe),
+  })
+    .then((response) => response.json())
+    .then(getRecipes);
+}
+
+const addForm = document.querySelector("#addForm");
+addForm.addEventListener("submit", addRecipe);
