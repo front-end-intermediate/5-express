d48e643b4e01c23e050319ea1a086b23591a99f0
Daniel Deverell <daniel.deverell@gmail.com>
2022-11-15 20:36:36 -0500

    done

---
+++ public/js/details.js
@@ -0,0 +1,51 @@
+function showDetail() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const recipeId = urlParams.get("recipe");
+
+  fetch(`api/recipes/${recipeId}`, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((recipe) => renderRecipe(recipe));
+}
+
+function renderRecipe(recipe) {
+  const { image, title, description } = recipe;
+  recipeEl = document.createElement("div");
+  recipeEl.innerHTML = `
+    <img src="img/${image}" />
+    <h3>${title}</h3>
+    <p>${description}</p>
+    <a href="/">Back</a>
+    `;
+
+  editForm.title.value = title;
+  editForm.image.value = image;
+  editForm.description.value = description;
+  document.querySelector(".recipe").append(recipeEl);
+}
+
+const updateRecipe = (event) => {
+  event.preventDefault();
+  const urlParams = new URLSearchParams(window.location.search);
+  const recipeId = urlParams.get("recipe");
+  const { title, image, description } = event.target;
+  const updatedRecipe = {
+    _id: recipeId,
+    title: title.value,
+    image: image.value,
+    description: description.value,
+  };
+  fetch(`api/recipes/${recipeId}`, {
+    method: "PUT",
+    body: JSON.stringify(updatedRecipe),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then(showDetail);
+};
+
+const editForm = document.querySelector("#editForm");
+editForm.addEventListener("submit", updateRecipe);
+
+showDetail();
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const recipeControllers = require("./api/recipe.controllers");
+
 const app = express();
 
 const dataBaseURL = process.env.DB_URL || "mongodb://localhost:27017";
@@ -9,56 +11,17 @@ mongoose
   .then(() => console.log("MongoDb connected"))
   .catch((err) => console.log(err));
 
-const RecipeSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  image: String,
-});
+app.use(express.static("public"));
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
-const Recipe = mongoose.model("Recipe", RecipeSchema);
-
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/static/index.html");
-});
-
-app.get("/api/recipes", function (req, res) {
-  Recipe.find({}, function (err, results) {
-    return res.send(results);
-  });
-});
-
-app.get("/api/import", (req, res) => {
-  Recipe.create(
-    {
-      title: "Lasagna",
-      description:
-        "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
-      image: "lasagna.png",
-    },
-    {
-      title: "Pho-Chicken Noodle Soup",
-      description:
-        'Pho (pronounced "fuh") is the most popular food in Vietnam, often eaten for breakfast, lunch and dinner. It is made from a special broth that simmers for several hours infused with exotic spices and served over rice noodles with fresh herbs.',
-      image: "pho.png",
-    },
-    {
-      title: "Guacamole",
-      description:
-        "Guacamole is definitely a staple of Mexican cuisine. Even though Guacamole is pretty simple, it can be tough to get the perfect flavor - with this authentic Mexican guacamole recipe, though, you will be an expert in no time.",
-      image: "guacamole.png",
-    },
-    {
-      title: "Hamburger",
-      description:
-        "A Hamburger (often called a burger) is a type of sandwich in the form of  rounded bread sliced in half with its center filled with a patty which is usually ground beef, then topped with vegetables such as lettuce, tomatoes and onions.",
-      image: "hamburger.png",
-    },
-    function (err) {
-      if (err) return console.log(err);
-      return res.sendStatus(201);
-    }
-  );
-});
+app.get("/api/recipes", recipeControllers.findAll);
+app.get("/api/recipes/:id", recipeControllers.findById);
+app.post("/api/recipes", recipeControllers.add);
+app.put("/api/recipes/:id", recipeControllers.update);
+app.delete("/api/recipes/:id", recipeControllers.delete);
+app.get("/api/import", recipeControllers.import);
+app.get("/api/killall", recipeControllers.killall);
 
 const PORT = process.env.PORT || 3000;
 
