52d7bc409f2b00f8cfe90f7555a520247fd612cd
DannyBoyNYC <daniel.deverell@gmail.com>
2024-04-11 11:01:02 -0400

    done

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+const RecipeSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  image: String,
+});
+
+const Recipe = mongoose.model("Recipe", RecipeSchema);
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/static/index.html");
+});
+
+app.get("/api/recipes", function (req, res) {
+  Recipe.find({}).then((err, results) => res.send(results));
+});
+
+app.get("/api/import", function (req, res) {
+  Recipe.create(
+    {
+      title: "Lasagna",
+      description:
+        "Lasagna noodles piled high and layered full of three kinds of cheese to go along with the perfect blend of meaty and zesty, tomato pasta sauce all loaded with herbs.",
+      image: "lasagna.png",
+    },
+    {
+      title: "Pho-Chicken Noodle Soup",
+      description:
+        'Pho (pronounced "fuh") is the most popular food in Vietnam, often eaten for breakfast, lunch and dinner. It is made from a special broth that simmers for several hours infused with exotic spices and served over rice noodles with fresh herbs.',
+      image: "pho.png",
+    },
+    {
+      title: "Guacamole",
+      description:
+        "Guacamole is definitely a staple of Mexican cuisine. Even though Guacamole is pretty simple, it can be tough to get the perfect flavor - with this authentic Mexican guacamole recipe, though, you will be an expert in no time.",
+      image: "guacamole.png",
+    },
+    {
+      title: "Hamburger",
+      description:
+        "A Hamburger (often called a burger) is a type of sandwich in the form of  rounded bread sliced in half with its center filled with a patty which is usually ground beef, then topped with vegetables such as lettuce, tomatoes and onions.",
+      image: "hamburger.png",
+    }
+  );
+});
+
+const PORT = process.env.PORT || 3000;
+const dataBaseURL = process.env.DB_URL || "mongodb://localhost:27017";
+
+mongoose
+  .connect(dataBaseURL, {})
+  .then(() => console.log("MongoDb connected"))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
